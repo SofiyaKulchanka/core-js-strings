1c670b2f829cd7ad5539296492a8e8722cc86763
SofiyaKulchanka <sofiykulchenko@gmail.com>
2023-11-24 15:20:34 +0100

    feat: add all tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -348,8 +348,24 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const upper = str.toUpperCase();
+  let vowels = 0;
+  for (let i = 0; i < upper.length; i += 1) {
+    if (
+      upper[i] === 'A' ||
+      upper[i] === 'E' ||
+      upper[i] === 'I' ||
+      upper[i] === 'O' ||
+      upper[i] === 'U' ||
+      upper[i] === 'Y'
+    ) {
+      vowels += 1;
+    } else {
+      vowels += 0;
+    }
+  }
+  return vowels;
 }
 
 /**
@@ -365,8 +381,17 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const cleanStr = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  let j = cleanStr.length - 1;
+
+  for (let i = 0; i < cleanStr.length / 2; i += 1) {
+    if (cleanStr[j] !== cleanStr[i]) {
+      return false;
+    }
+    j -= 1;
+  }
+  return true;
 }
 
 /**
@@ -381,8 +406,12 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const words = sentence.split(' ');
+
+  return words.reduce((longestWord, currentWord) => {
+    return currentWord.length > longestWord.length ? currentWord : longestWord;
+  }, '');
 }
 
 /**
@@ -395,8 +424,13 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const words = str.split(' ');
+
+  const reversedWords = words.map((word) => reverseString(word));
+  const reversedString = reversedWords.join(' ');
+
+  return reversedString;
 }
 
 /**
@@ -410,8 +444,13 @@ function reverseWords(/* str */) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((array) =>
+      array === array.toUpperCase() ? array.toLowerCase() : array.toUpperCase()
+    )
+    .join('');
 }
 
 /**
@@ -427,8 +466,8 @@ function invertCase(/* str */) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -441,8 +480,8 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.replace('Hello, ', '').replace('!', '');
 }
 
 /**
@@ -456,8 +495,10 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  const last = str.indexOf('>');
+  const first = str.indexOf('<');
+  return str.slice(first + 1, last);
 }
 
 /**
@@ -476,7 +517,8 @@ function unbracketTag(/* str */) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-  
+  const array = str.split(';');
+  return array;
 }
 
 /**
